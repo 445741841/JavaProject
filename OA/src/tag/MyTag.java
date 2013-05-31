8d4fe816c6ce7da0a600ece29431a14aee33de32
Chixin <445741841@qq.com>
2013-05-31 16:08:31 +0800

    Signed-off-by: Chixin <445741841@qq.com>

---
+++ OA/src/tag/MyTag.java
@@ -0,0 +1,24 @@
+package tag;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class MyTag extends TagSupport
+{
+	private static final long serialVersionUID = -454408927785704516L;
+	
+	@Override
+	public int doStartTag() throws JspException 
+	{
+		// TODO Auto-generated method stub
+		return super.doStartTag();
+	}
+	
+	@Override
+	public int doEndTag() throws JspException 
+	{
+		// TODO Auto-generated method stub
+		return super.doEndTag();
+	}
+
+}
